98b826e0dd6e41c3033c050ad6b422bd7f7b5a71
Alien Ware <jacmg01@gmail.com>
2022-03-15 16:22:06 -0600

    commit con babel implementado

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,59 @@
 const path = require('path');
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    
+    mode: "development", // development  (nomin), prod(min)
+    devtool: false,
     entry: {
         index: "./src/index.js"
      },
     output: {
         filename: "bundle.js",
-        path: path.resolve(__dirname, "dist"),
-    },
+        path: path.resolve(__dirname, "bundle_ConBabel"),
+    }, 
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: "main.css" }),
+        new HtmlWebpackPlugin({
+          template: "./src/index.html",
+        }),
+    ],
     
-
     module: {
         rules: [
-          {
-            test: /\.s[ac]ss$/i,
-            use: [
-              // Creates `style` nodes from JS strings
-              "style-loader",
-              // Translates CSS into CommonJS
-              "css-loader",
-              // Compiles Sass to CSS
-              "sass-loader",
-            ],
-          },
-          {
-          test: /\.(gif|png|jpe?g)$/,
-          use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'img/',
-                  publicPath: 'img/'
-                }
-              }
-            ],
-          },
-          {
-            test: /\.mp3$/,
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'aud/',
-              publicPath: 'aud/'
-            }
-          },
-        ],
-      },
-}
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader","sass-loader"]
+            },
+
+            {
+                test: /\.(gif|png|jpe?g)$/,
+                use: [
+                    {
+                      loader: 'file-loader',
+                      options: {
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        publicPath: 'img/'
+                      }
+                    }
+                  ],
+                },
+                {
+                  test: /\.js$/,
+                  exclude: /node_modules/,
+                  use: "babel-loader",
+                },
+                {
+                  test: /\.mp3$/,
+                  loader: 'file-loader',
+                  options: {
+                    name: '[name].[ext]',
+                    outputPath: 'aud/',
+                    publicPath: 'aud/'
+                  }
+                },
+              ],
+    }}
